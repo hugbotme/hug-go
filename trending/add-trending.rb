059d1e42d9bf3b6362b2b087046460a5d0c6cd55
Jan-Erik Rediger <janerik@fnordig.de>
2015-05-23 16:55:59 +0200

    Add trendings

---
+++ trending/add-trending.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'rubygems'
+require 'bundler/setup'
+require 'redic'
+require 'github-trending'
+
+def canonicalize owner, repo
+  "http://github.com/%s/%s" % [owner, repo]
+end
+
+redis = Redic.new
+
+repos = Github::Trending.get
+repos.each do |r|
+  owner, repo = r.name.split("/")
+  redis.call "SADD", "hugbot:bored-urls", canonicalize(owner, repo)
+end
